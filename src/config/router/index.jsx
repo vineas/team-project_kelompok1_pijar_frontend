5e636cbfc3a3abcb9732779253c22339c11047df
vineas <alvienasyandika@gmail.com>
2023-07-19 14:50:38 +0700

    commit

--- src/config/router/index.jsx
+++ src/config/router/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Profile from '../../pages/Profile'
+import Profile from '../../pages/Home'
 
 const index = () => {
   return (
     <>
-        
+        <BrowserRouter>
+          <Routes>
+          <Route path='/' element={<Home/>}/>
+            <Route path='/profile' element={<Profile/>}/>
+            <Route path='/home' element={<Home/>}/>
+          </Routes>
+        </BrowserRouter>
     </>
   )
 }
