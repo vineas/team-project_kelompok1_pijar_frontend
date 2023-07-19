02315986d7338839dfea411f62fc47c39b2fb63f
yabeselkana <yabeszega1997@gmail.com>
2023-07-19 14:44:41 +0700

    repo ulang

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+import Router from "./config/router";
+function App() {
+  return (
+    <Fragment>
+      <Router></Router>
+    </Fragment>
+  );
+}
+
+export default App;
--- src/config/router/index.jsx
+++ src/config/router/index.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const index = () => {
   return (
     <>
-        
+      <div>
+        <h1> Hallo Yabes</h1>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default index
+export default index;
