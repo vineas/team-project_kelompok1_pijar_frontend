cf32bebd041f0b44fde87237289967497b99ef46
yabeselkana <yabeszega1997@gmail.com>
2023-07-20 00:01:58 +0700

    new repo detail recep

--- src/config/router/index.jsx
+++ src/config/router/index.jsx
@@ -1,36 +1,22 @@
-<<<<<<< HEAD
 import React from "react";
-=======
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Profile from '../../pages/Profile'
-import Home from '../../pages/Home'
->>>>>>> d4700155e1d04c38b07afec028869d8e50db7c4a
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Profile from "../../pages/Profile";
+import Home from "../../pages/Home";
+import DetailResep from "../../pages/DetailResep";
 
 const index = () => {
   return (
     <>
-<<<<<<< HEAD
-      <div>
-        <h1> Hallo Yabes</h1>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/detailresep" element={<DetailResep />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
 
 export default index;
-=======
-        <BrowserRouter>
-          <Routes>
-          <Route path='/' element={<Home/>}/>
-            <Route path='/home' element={<Home/>}/>
-            <Route path='/profile' element={<Profile/>}/>
-
-          </Routes>
-        </BrowserRouter>
-    </>
-  )
-}
-
-export default index
->>>>>>> d4700155e1d04c38b07afec028869d8e50db7c4a
---
+++ src/pages/DetailResep.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DetailResepIsi from "../component/DetailResep/DetailResepIsi";
+
+const DetailResep = () => {
+  return (
+    <>
+      <DetailResepIsi />
+    </>
+  );
+};
+
+export default DetailResep;
