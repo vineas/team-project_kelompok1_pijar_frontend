52fbaa60d58a08555a3a8db7afd836e4d42368e7
vineas <alvienasyandika@gmail.com>
2023-07-19 16:10:53 +0700

    rapihin develop 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
-import { Fragment } from "react";
-import Router from "./config/router";
-function App() {
-  return (
-    <Fragment>
-      <Router></Router>
-    </Fragment>
-=======
-// import logo from './logo.svg';
-// import './App.css';
 import Router from './config/router'
 
 function App() {
   return (
+    <>
     <Router/>
-      
->>>>>>> d4700155e1d04c38b07afec028869d8e50db7c4a
+    </>
   );
 }
 
